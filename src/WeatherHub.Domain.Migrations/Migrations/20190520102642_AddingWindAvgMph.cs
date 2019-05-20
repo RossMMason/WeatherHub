3957c84f8030ee419a323a2c0228a73a06cc2cbc
Ross Mason <ross.mason@inflecto.co.uk>
2019-05-20 11:34:58 +0100

    Adding Wind Avg Mph.

---
+++ src/WeatherHub.Domain.Migrations/Migrations/20190520102642_AddingWindAvgMph.cs
@@ -0,0 +1,37 @@
+﻿// <copyright file="20190520102642_AddingWindAvgMph.cs" company="Ross Mason">
+// Licensed under the MIT License. See LICENSE file in the project root for full license information.
+// </copyright>
+
+namespace WeatherHub.Domain.Migrations.Migrations
+{
+    using Microsoft.EntityFrameworkCore.Migrations;
+
+    public partial class AddingWindAvgMph : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "WindGustMph",
+                table: "StationReading",
+                newName: "WindAvgGustMph");
+
+            migrationBuilder.AddColumn<float>(
+                name: "WindAvgMph",
+                table: "StationReading",
+                nullable: false,
+                defaultValue: 0f);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WindAvgMph",
+                table: "StationReading");
+
+            migrationBuilder.RenameColumn(
+                name: "WindAvgGustMph",
+                table: "StationReading",
+                newName: "WindGustMph");
+        }
+    }
+}
