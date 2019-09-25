3036c38af23f9ee507a27fda0d64fb3ef900a945
Ross Mason <ross.mason@inflecto.co.uk>
2019-09-25 11:21:28 +0100

    Completed live updates using SignalR.

--- src/WeatherHub.FrontEnd/ts/Types.ts
+++ src/WeatherHub.FrontEnd/ts/Types.ts
@@ -1,4 +1,8 @@
-﻿export interface StationReading extends TimeIndexed  {
+﻿import {
+    addHours, format, parse
+} from 'date-fns';
+
+export interface StationReading extends TimeIndexed  {
     id: string, 
     when: Date, 
     dewpointC: number, 
@@ -100,4 +104,67 @@ export interface StationDayStatisticsDto {
     windChillLowTime: string,
     windHighMph: number,
     windHighTime: string
+}
+
+export class DtoConverter {
+    public toStationReading(stationReadingDto: StationReadingDto) : StationReading {
+        let stationReading: StationReading = {
+            id: stationReadingDto.id,
+            when: new Date(stationReadingDto.when),
+            dewpointC: stationReadingDto.dewpointC,
+            heatIndexC: stationReadingDto.heatIndexC,
+            pressureMb: stationReadingDto.pressureMb,
+            relativeHumidity: stationReadingDto.relativeHumidity,
+            tempC: stationReadingDto.tempC,
+            windDegrees: stationReadingDto.windDegrees,
+            windAvgMph: stationReadingDto.windAvgMph,
+            windAvgGustMph: stationReadingDto.windAvgGustMph,
+            windChillC: stationReadingDto.windChillC,
+            rainCmPerHour: stationReadingDto.rainCmPerHour,
+            estimatedCloudBaseFt: stationReadingDto.estimatedCloudBaseFt
+        };
+
+        return stationReading;
+    }
+
+    public toStationStatistics(stationStatisticsDto: StationStatisticsDto) : StationStatistics {
+        let date = new Date(stationStatisticsDto.dayStatistics.date);
+        let dateStr = format(date, 'YYYY-MM-DD');
+
+        let stationDayStatistics: StationDayStatistics = {
+            id: stationStatisticsDto.dayStatistics.id,
+            date: date,
+            dewpointHighC: stationStatisticsDto.dayStatistics.dewpointHighC,
+            dewpointHighTime: parse(dateStr + 'T' + stationStatisticsDto.dayStatistics.dewpointHighTime),
+            dewpointLowC: stationStatisticsDto.dayStatistics.dewpointLowC,
+            dewpointLowTime: parse(dateStr + 'T' + stationStatisticsDto.dayStatistics.dewpointLowTime),
+            heatIndexHighC: stationStatisticsDto.dayStatistics.heatIndexHighC,
+            heatIndexHighTime: parse(dateStr + 'T' + stationStatisticsDto.dayStatistics.heatIndexHighTime),
+            pressureHighMbar: stationStatisticsDto.dayStatistics.pressureHighMbar,
+            pressureHighTime: parse(dateStr + 'T' + stationStatisticsDto.dayStatistics.pressureHighTime),
+            pressureLowMbar: stationStatisticsDto.dayStatistics.pressureLowMbar,
+            pressureLowTime: parse(dateStr + 'T' + stationStatisticsDto.dayStatistics.pressureLowTime),
+            rainRateHighCmPerHour: stationStatisticsDto.dayStatistics.rainRateHighCmPerHour,
+            relativeHumidityHigh: stationStatisticsDto.dayStatistics.relativeHumidityHigh,
+            relativeHumidyHighTime: parse(dateStr + 'T' + stationStatisticsDto.dayStatistics.relativeHumidyHighTime),
+            relativeHumidityLow: stationStatisticsDto.dayStatistics.relativeHumidityLow,
+            relativeHumidyLowTime: parse(dateStr + 'T' + stationStatisticsDto.dayStatistics.relativeHumidyLowTime),
+            tempHighC: stationStatisticsDto.dayStatistics.tempHighC,
+            tempHighTime: parse(dateStr + 'T' + stationStatisticsDto.dayStatistics.tempHighTime),
+            tempLowC: stationStatisticsDto.dayStatistics.tempLowC,
+            tempLowTime: parse(dateStr + 'T' + stationStatisticsDto.dayStatistics.tempLowTime),
+            totalRainCm: stationStatisticsDto.dayStatistics.totalRainCm,
+            windChillLowC: stationStatisticsDto.dayStatistics.windChillLowC,
+            windChillLowTime: parse(dateStr + 'T' + stationStatisticsDto.dayStatistics.windChillLowTime),
+            windHighMph: stationStatisticsDto.dayStatistics.windHighMph,
+            windHighTime: parse(dateStr + 'T' + stationStatisticsDto.dayStatistics.windHighTime),
+        };
+
+        let statistics: StationStatistics = {
+            dayStatistics: stationDayStatistics,
+            lastRain: stationStatisticsDto.lastRain
+        }
+
+        return statistics;
+    }
 }
